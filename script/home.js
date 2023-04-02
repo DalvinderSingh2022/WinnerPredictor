01f602895b5303a42d5b02afe6ce8e8ddca035b9
DalvinderSingh2022 <dalvindersingh201999@gmail.com>
2023-04-02 17:15:16 +0530

    Minor changes in css

--- script/home.js
+++ script/home.js
@@ -1,13 +1,13 @@
 import { Schedule } from "./data.js";
-import { LoadMatchVoters, LoadMatches, LoadMatchToVote, nextGameId } from "./index.js";
+import { LoadMatchVoters, LoadMatches, LoadMatchToVote, nextGameIndex } from "./index.js";
 
-const Game = Schedule[nextGameId()];
+const Game = Schedule[nextGameIndex()];
 const GameEl = document.querySelector(".game");
 const votersEl = document.querySelector(".voters");
 
 var nextMatches = [];
 for (let i = 1; i < 5; i++) {
-    nextMatches.push(Schedule[Number(nextGameId()) + i]);
+    nextMatches.push(Schedule[Number(nextGameIndex()) + i]);
 }
 
 LoadMatches(nextMatches);
